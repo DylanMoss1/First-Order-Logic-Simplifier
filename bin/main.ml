edb95c0635b6cb3f9b225ec78aefb225f36cdadd
Dylan Moss <65402135+DylanMoss1@users.noreply.github.com>
2022-09-06 21:03:09 +0100

    Update main.ml

--- bin/main.ml
+++ bin/main.ml
@@ -17,12 +17,7 @@ let with_clauses x =
   Ast.pprint_clauses x;
   x
 
-(*
-CHECK CNF OR NNF!!!
-*)
-
-let x = convert_to_nnf ("(∀x P(x)) ∧ (∀x Q(x)) ∨ ∃x(P(x) → ¬Q(x))")
-
+(* let x = convert_to_nnf ("(∀x P(x)) ∧ (∀x Q(x)) ∨ ∃x(P(x) → ¬Q(x))") *)
 (* let x = map_list (fun x -> let y = convert_to_cnf x in with_clauses y) (["P → Q ∧ R"; "P ∧ Q → S"; "¬R ↔ S"; "P ∨ Q"]) *) 
 (* let x = with_clauses (convert_to_cnf ("P | Q -> Q | R")) *)
 (* let x = map_list (convert_to_cnf) (["P | Q -> Q | R"; "P & R | Q"]) *)
